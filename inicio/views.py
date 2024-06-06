ab259d2718ce3a3115d64629d45bd9721a15481b
rafadil <rafaeldilelle98@gmail.com>
2024-06-06 20:40:19 -0300

    Agregue Template 1

--- inicio/views.py
+++ inicio/views.py
@@ -1,4 +1,10 @@
+from django.shortcuts import render
+
 from django.http import HttpResponse
 
 def inicio(request):
     return HttpResponse('Bienvenidos a mi inicio')
+
+
+def template1(request):
+    return HttpResponse('Mi Template 1')
